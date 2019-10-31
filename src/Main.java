e323c233b79caa9e629a0f8c496749f4edd83f38
fenglin098 <feng.lin098@gmail.com>
2019-10-31 10:50:11 -0400

    third commit

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,9 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
-        String[] meat = new String[]{"chicken", "steak", "carnidas", "chorizo", "sofritas", "veggies", "none", "all"};
-        String[] beans = new String[]{"pinto", "black", "none"};
-        String[] salsa = new String[]{"mild", "medium", "hot", "none","all"};
-        String[] veggies = new String[]{"lettuce", "fajita veggies", "none", "all"};
-        String[] cheese = new String[]{"yes", "no"};
-        String[] guac = new String[]{"yes", "no"};
-        String[] queso = new String[]{"yes", "no"};
-        String[] sourcream = new String[]{"yes", "no"};
-        Random r = new Random();
-        System.out.println(riceM());
-//        int randrice = r.nextInt(rice.length);
-        int randmeat = r.nextInt(meat.length);
-        int randbeans = r.nextInt(beans.length);
-        int randsalsa = r.nextInt(salsa.length);
-        int randveggies = r.nextInt(veggies.length);
-        int randcheese = r.nextInt(cheese.length);
-        int randguac = r.nextInt(guac.length);
-        int randqueso = r.nextInt(queso.length);
-        int randsourcream = r.nextInt(sourcream.length);
-
-//        for (int i=1; i<26; i++){
-//            String riceChoice = rice[randrice];
-//            String meatChoice = meat[randmeat];
-//            String beansChoice = beans[randbeans];
-//            String salsaChoice = salsa[randsalsa];
-//            String veggiesChoice = veggies[randveggies];
-//            String cheeseChoice = cheese[randcheese];
-//            String guacChoice = guac[randguac];
-//            String quesoChoice = queso[randqueso];
-//            String sourcreamChoice = sourcream[randsourcream];
-////            System.out.println("Burrito " + i+": " +riceChoice+", "+meatChoice+", "+beansChoice+", "+salsaChoice+", "+veggiesChoice+", "+cheeseChoice+", "+guacChoice+", "+quesoChoice+", "+sourcreamChoice+".");
-//        }
-////        System.out.println(Arrays.toString(rice));
-
+        for (int i=0; i<25; i++){
+            System.out.println("Burrito "+i+" "+riceM() + " ," + meatM() + " ," + beansM() + " ," + salsaM() + " ," + veggiesM() + " ," + cheeseM() + " ," + guacM() + " ," + quesoM() + " ," + sourcreamM());
+        }
     }
 
     public static String riceM(){
@@ -50,5 +18,91 @@ public class Main {
         }String riceChoice=rice[randRice]+" rice";
         return riceChoice;
     }
+
+    public static String meatM(){
+        String[] meat = new String[]{"chicken", "steak", "carnidas", "chorizo", "sofritas", "veggies", "none", "all"};
+        Random r = new Random();
+        int randMeat = r.nextInt(meat.length);
+        if (meat[randMeat].equals("none")){
+            meat[randMeat]="no meat";
+        }String meatChoice=meat[randMeat];
+        return meatChoice;
+    }
+
+    public static String beansM(){
+        String[] beans = new String[]{"pinto", "black", "none"};
+        Random r = new Random();
+        int randBeans = r.nextInt(beans.length);
+        if (beans[randBeans].equals("none")){
+            beans[randBeans]="no";
+        }String beansChoice=beans[randBeans]+" bean";
+        return beansChoice;
+    }
+
+    public static String salsaM(){
+        String[] salsa = new String[]{"mild", "medium", "hot", "none","all"};
+        Random r = new Random();
+        int randSalsa = r.nextInt(salsa.length);
+        if (salsa[randSalsa].equals("none")){
+            salsa[randSalsa]="no";
+        }String salsaChoice=salsa[randSalsa]+" salsa";
+        return salsaChoice;
+    }
+
+    public static String veggiesM(){
+        String[] veggies = new String[]{"lettuce", "fajita veggies", "none", "all"};
+        Random r = new Random();
+        int randVeggies = r.nextInt(veggies.length);
+        if (veggies[randVeggies].equals("none")){
+            veggies[randVeggies]="no";
+        }String veggiesChoice=veggies[randVeggies]+" veggies";
+        return veggiesChoice;
+
+    }
+
+    public static String cheeseM(){
+        String[] cheese = new String[]{"yes", "no"};
+        Random r = new Random();
+        int randCheese = r.nextInt(cheese.length);
+        if (cheese[randCheese].equals("yes")){
+            cheese[randCheese]="";
+        }String cheeseChoice=cheese[randCheese]+" cheese";
+        return cheeseChoice;
+    }
+
+    public static String guacM(){
+        String[] guac = new String[]{"yes", "no"};
+        Random r = new Random();
+        int randGuac = r.nextInt(guac.length);
+        if (guac[randGuac].equals("yes")){
+            guac[randGuac]="";
+        }String guacChoice=guac[randGuac]+" guac";
+        return guacChoice;
+    }
+
+
+    public static String quesoM(){
+        String[] queso = new String[]{"yes", "no"};
+        Random r = new Random();
+        int randQueso = r.nextInt(queso.length);
+        if (queso[randQueso].equals("yes")){
+            queso[randQueso]="";
+        }String quesoChoice=queso[randQueso]+" queso";
+        return quesoChoice;
+
+    }
+
+    public static String sourcreamM(){
+        String[] sourcream = new String[]{"yes", "no"};
+        Random r = new Random();
+        int randSourcream = r.nextInt(sourcream.length);
+        if (sourcream[randSourcream].equals("yes")){
+            sourcream[randSourcream]="";
+        }String sourcreamChoice=sourcream[randSourcream]+" sour cream";
+        return sourcreamChoice;
+
+    }
+
+
 
 }
